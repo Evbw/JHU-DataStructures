dc79ea28c9db114da706a475a8a646f27b5681f3
Everett <everett.bowline@gmail.com>
2024-09-08 10:51:22 -0400

    Added debugging for file opening method

--- Lab1.cpp
+++ Lab1.cpp
@@ -6,13 +6,27 @@ using namespace std;
 void openInputFile(ifstream& inFile) {
     string filename;
     cout<<"Enter the filename: "<<endl;
-    cin>>filename;
+    getline(cin,filename);
     inFile.open(filename);
     while (!inFile) {
         cout<<"File failed to open."<<endl;
         cout<<"Enter the filename: "<<endl;
-        cin>>filename;
+        getline(cin,filename);
         inFile.clear();
         inFile.open(filename);
     }
+}
+
+int main() {
+    ifstream inFile;
+    openInputFile(inFile);
+
+    if(inFile) {
+        cout<<"Success opening the file!"<<endl;
+    }
+    else {
+        cout<<"Try again"<<endl;
+    }
+    inFile.clear();
+    return 0;
 }
