c4700441940a88ae21c0cf7795f874852659be03
Everett <everett.bowline@gmail.com>
2024-10-29 14:11:35 -0400

    Added Node class

--- Lab3.cpp
+++ Lab3.cpp
@@ -3,6 +3,16 @@
 #include <fstream>
 using namespace std;
 
+class Node {
+    char character;
+    int frequency;
+    Node* left;
+    Node* right;
+
+    Node(char c, int freq) : character(c), frequency(freq), left(nullptr), right(nullptr) {}
+    Node(int freq, Node* l, Node* r) : character('\0'), frequency(freq), left(l), right(r) {}
+};
+
 int main() {
 
     return 0;
