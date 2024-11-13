f707ae5e0754196349019cb58793507dc5711ea5
Everett <everett.bowline@gmail.com>
2024-11-13 17:09:19 -0500

    Reworked partition method to what I originally conceived instead of what I'm used to

--- Lab4.cpp
+++ Lab4.cpp
@@ -10,8 +10,67 @@ class Node{
         Node(int val) : data(val), next(nullptr){}
 };
 
-void quickSortPivot1(int arr[], int left, int right) {
+int partition(int arr[], int left, int right, int pivotType) {
+    int pivot;
+    int i = left + 1;
+    int j = right;
 
+    switch (pivotType){
+        case 1:
+            pivot = arr[left];
+            break;
+        case 2:
+            if (right - left + 1 <= 100) {
+                insertionSort(arr, left, right);
+                return left;
+            }
+            pivot = arr[left];
+            break;
+        case 3:
+            if (right - left + 1 <= 50) {
+                insertionSort(arr, left, right);
+                return left;
+            }
+            pivot = arr[left];
+            break;
+        case 4:
+            int mid = (left + right)/2;
+            if ((arr[left] < arr[mid] && arr[mid] < arr[right]) || (arr[right] < arr[mid] && arr[mid] < arr[left])) {
+                pivot = arr[mid];
+                swap(arr[left], arr[mid]);
+            } else if ((arr[mid] < arr[left] && arr[left] < arr[right]) || (arr[right] < arr[left] && arr[left] < arr[mid])) {
+                pivot = arr[left];
+            } else {
+                pivot = arr[right];
+                swap(arr[left], arr[right]);
+            }
+            break;
+        default:
+            pivot = arr[left];
+    }
+
+    while (i <= j) {
+        while (i <= j && arr[i] < pivot) {
+            i++;
+        }
+        while (i <= j && arr[j] > pivot) {
+            j--;
+        }
+        if (i < j) {
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[left], arr[j]);
+    return j;    
+}
+
+void quickSortPivot1(int arr[], int left, int right) {
+    if (left < right) {
+        int pivotIndex = partition(arr, left, right, 1);
+        quickSortPivot1(arr, left, pivotIndex-1);
+        quickSortPivot1(arr, pivotIndex+1, right);
+    }
 }
 
 void quickSortPivot100(int arr[], int left, int right) {
@@ -24,29 +83,6 @@ void quickSortPivot50(int arr[], int left, int right) {
 
 void quickSortPivotMedian(int arr[], int left, int right) {
     
-}
-
-int partition(int arr[], int left, int right, int pivotType) {
-    int pivot = arr[left];
-    int i = left + 1;
-    int j = right;
-
-    while (i <= j) {
-        while (i <= j && arr[i] < pivot) {
-            i++;
-        }
-
-        while (i <= j && arr[j] > pivot) {
-            j--;
-        }
-
-        if (i < j) {
-            swap(arr[i], arr[j]);
-        }
-    }
-
-    swap(arr[left], arr[j]);
-    return j;    
 }
 
 void swap(int &a, int &b) {
