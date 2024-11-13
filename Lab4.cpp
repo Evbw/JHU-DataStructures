1f3554b4f24cb55064f0c42cd7745ed298075ff5
Everett <everett.bowline@gmail.com>
2024-11-13 14:17:34 -0500

    Create file with initial methods

---
+++ Lab4.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class Node{
+
+};
+
+void quickSortPivot1(int arr[], int left, int right) {
+
+}
+
+void quickSortPivot100(int arr[], int left, int right) {
+    
+}
+
+void quickSortPivot50(int arr[], int left, int right) {
+    
+}
+
+void quickSortPivotMedian(int arr[], int left, int right) {
+    
+}
+
+int partition(int arr[], int left, int right, int pivotType) {
+
+}
+
+void mergeSort(Node* root) {
+
+}
+
+void splitIntoRuns(Node* root, Node* next) {
+
+}
+
+Node* sortedMerge(Node* a, Node* b) {
+
+}
+
+void insert(Node*& head, int data) {
+
+}
+
+int main() {
+
+}
